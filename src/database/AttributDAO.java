45d00aeae9faf056a70e14156bccff10e798ae92
Antoine Cronier <antoine.cronier@tactfactory.com>
2016-12-12 15:05:18 +0100

    progress

--- src/database/AttributDAO.java
+++ src/database/AttributDAO.java
@@ -14,63 +14,88 @@ public class AttributDAO implements IGenericDAO<Attribut> {
 	@Override
 	public ArrayList<Attribut> SelectAll() {
 		List<Attribut> attributs = new LinkedList<Attribut>();
-        try {
-               Statement statement = DatabaseAccess.getConnection().createStatement();
-               ResultSet resultSet = statement.executeQuery("SELECT * FROM " + DatabaseAccess.DB + ".Attribut ;");
+		try {
+			Statement statement = DatabaseAccess.getConnection()
+					.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM "
+					+ DatabaseAccess.DB + ".Attribut ;");
 
-               Attribut attribut = null;
-               while(resultSet.next()){
-            	   attribut = new Attribut();
-            	   attribut.setAttribut_id(Integer.parseInt(resultSet.getString("id_types")));
-            	   attribut.setName(resultSet.getString("nom"));
-            	   attribut.setValue(Integer.parseInt(resultSet.getString("id_types")));
-            	   attributs.add(attribut);
-               }
-               resultSet.close();
-               statement.close();
+			Attribut attribut = null;
+			while (resultSet.next()) {
+				attribut = new Attribut();
+				attribut.setAttribut_id(Integer.parseInt(resultSet
+						.getString("id_types")));
+				attribut.setName(resultSet.getString("nom"));
+				attribut.setValue(Integer.parseInt(resultSet
+						.getString("id_types")));
+				attributs.add(attribut);
+			}
+			resultSet.close();
+			statement.close();
 
-           } catch (SQLException e) {
-               e.printStackTrace();
-           }
-           ArrayList<Attribut> returnAttributs = new ArrayList<Attribut>(attributs);
-           return returnAttributs;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		ArrayList<Attribut> returnAttributs = new ArrayList<Attribut>(attributs);
+		return returnAttributs;
 	}
 
 	@Override
 	public Attribut Select(Integer id) {
-		List<Attribut> attributs = new LinkedList<Attribut>();
-        try {
-               Statement statement = DatabaseAccess.getConnection().createStatement();
-               ResultSet resultSet = statement.executeQuery("SELECT * FROM " + DatabaseAccess.DB + ".Attribut ;");
+		Attribut attribut = new Attribut();
+		try {
+			Statement statement = DatabaseAccess.getConnection()
+					.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM "
+					+ DatabaseAccess.DB + ".Attribut " + "WHERE Attribut. = "
+					+ id + ";");
 
-               Attribut attribut = null;
-               while(resultSet.next()){
-            	   attribut = new Attribut();
-            	   attribut.setAttribut_id(Integer.parseInt(resultSet.getString("id_types")));
-            	   attribut.setName(resultSet.getString("nom"));
-            	   attribut.setValue(Integer.parseInt(resultSet.getString("id_types")));
-            	   attributs.add(attribut);
-               }
-               resultSet.close();
-               statement.close();
+			resultSet.next();
 
-           } catch (SQLException e) {
-               e.printStackTrace();
-           }
-           ArrayList<Attribut> returnAttributs = new ArrayList<Attribut>(attributs);
-           return returnAttributs;
+			attribut.setAttribut_id(Integer.parseInt(resultSet
+					.getString("id_types")));
+			attribut.setName(resultSet.getString("nom"));
+			attribut.setValue(Integer.parseInt(resultSet.getString("id_types")));
+
+			resultSet.close();
+			statement.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return attribut;
 	}
 
 	@Override
 	public void DeleteAll() {
-		// TODO Auto-generated method stub
+		try {
+			Statement statement = DatabaseAccess.getConnection()
+					.createStatement();
+			ResultSet resultSet = statement.executeQuery("DELETE FROM "
+					+ DatabaseAccess.DB + ".Attribut;");
 
+			resultSet.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void Delete(Integer id) {
-		// TODO Auto-generated method stub
+		try {
+			Statement statement = DatabaseAccess.getConnection()
+					.createStatement();
+			ResultSet resultSet = statement.executeQuery("DELETE FROM "
+					+ DatabaseAccess.DB + ".Attribut WHERE Attribut. = " + id
+					+ " ;");
 
+			resultSet.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -81,7 +106,16 @@ public class AttributDAO implements IGenericDAO<Attribut> {
 
 	@Override
 	public Attribut Insert(Attribut item) {
-		// TODO Auto-generated method stub
+		try {
+			Statement statement = DatabaseAccess.getConnection()
+					.createStatement();
+			ResultSet resultSet = statement.executeQuery(";");
+
+			resultSet.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
