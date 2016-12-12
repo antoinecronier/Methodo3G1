76ccdbe1ef1e792415225a4adc75fcf82326b7cc
Antoine <Gesreta@gmail.com>
2016-12-12 12:13:39 +0100

    modification attributs.java

--- src/entite/Attribut.java
+++ src/entite/Attribut.java
@@ -1,23 +1,23 @@
 package entite;
 
-public class Attributs {
-	private Integer attributs_id;
+public class Attribut {
+	private Integer attribut_id;
 	private String name;
 	private Integer value;
 	
-	public Attributs(Integer attributs_id, String name, Integer value){
-		this.attributs_id = attributs_id;
+	public Attribut(Integer attribut_id, String name, Integer value){
+		this.attribut_id = attribut_id;
 		this.name = name;
 		this.value = value;
 	
 	}
 
-	public Integer getAttributs_id() {
-		return attributs_id;
+	public Integer getAttribut_id() {
+		return attribut_id;
 	}
 
-	public void setAttributs_id(Integer attributs_id) {
-		this.attributs_id = attributs_id;
+	public void setAttribut_id(Integer attribut_id) {
+		this.attribut_id = attribut_id;
 	}
 
 	public String getName() {
