24e53d3ca89e4c0d6cc4238353618750eabb6635
Fabien JUILLARD <fabienJuillard>
2016-12-14 16:35:51 +0100

    75%

--- src/calendrier/CalendrierReservation.java
+++ src/calendrier/CalendrierReservation.java
@@ -13,11 +13,20 @@ import com.toedter.calendar.JDateChooser;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 
+import database.DatabaseAccess;
+import database.GenericDAO;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
 public class CalendrierReservation {
 	private JFrame frame;
 	private final JButton button = new JButton("Valider");
 	private JCalendar calendar;
 	private Date selectedDate = new Date();
+	private String ResultSet;
+	private JTextField txtCliquDroite;
 	/**
 	 * Launch the application.
 	 */
@@ -65,12 +74,45 @@ public class CalendrierReservation {
 			}
 		});
 		calendar.add(chooser);
+		
+		JButton btnRetour = new JButton("Retour");
+		
+		txtCliquDroite = new JTextField();
+		txtCliquDroite.setText("Cliqu\u00E9 \u00E0 droite pour choisir une date de match");
+		txtCliquDroite.setColumns(10);
+		GroupLayout gl_chooser = new GroupLayout(chooser);
+		gl_chooser.setHorizontalGroup(
+			gl_chooser.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_chooser.createSequentialGroup()
+					.addGroup(gl_chooser.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnRetour)
+						.addGroup(gl_chooser.createSequentialGroup()
+							.addGap(113)
+							.addComponent(txtCliquDroite, GroupLayout.PREFERRED_SIZE, 268, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(chooser.getCalendarButton(), GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE))
+		);
+		gl_chooser.setVerticalGroup(
+			gl_chooser.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_chooser.createSequentialGroup()
+					.addGroup(gl_chooser.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_chooser.createParallelGroup(Alignment.LEADING)
+							.addComponent(btnRetour, Alignment.TRAILING)
+							.addComponent(chooser.getCalendarButton(), GroupLayout.PREFERRED_SIZE, 218, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_chooser.createSequentialGroup()
+							.addGap(12)
+							.addComponent(txtCliquDroite, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		chooser.setLayout(gl_chooser);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println(selectedDate);
 				SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
 				String reformattedStr = myFormat.format(selectedDate);
 				System.out.println(reformattedStr);
+				
+				//java.sql.ResultSet result = GenericDAO.executeUpdate("INSERT `Match` SET `date` = "+ reformattedStr ", `cli_id` = 22 WHERE `id` = 92;");
 			}
 		});
 		calendar.add(button, BorderLayout.SOUTH);
